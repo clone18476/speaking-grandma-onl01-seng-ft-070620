273830db8443ffe3b151a0ddc3d9c6971f70b7c9
Cameron Cousins <cameroncousins18@gmail.com>
2020-07-25 18:11:38 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -18,6 +18,16 @@ if "hello"
   puts "in if"
 end
 
+def speak_to_grandma(phrase)
+  if phrase.upcase != phrase   #whatever you say, if it's not upcase this returns true, so she responds with huh speak up 
+  "HUH?! SPEAK UP, SONNY!"  
+  elsif phrase == "I LOVE YOU GRANDMA!"  # just tell her you love her, she'll get the idea
+    "I LOVE YOU TOO PUMPKIN!"
+  else 
+    "NO, NOT SINCE 1938"    # if you shout she can hear you, but doesn't know what you're saying
+  end 
+end 
+
 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
